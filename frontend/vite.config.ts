b58cb9dab207be97a8411c1d592bec3644387dab
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2026-03-20 17:07:12 +0000

    Configure for GitHub Pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/scoop-shop-replica-project/',
   build: {
     outDir: '../dist',
     rollupOptions: {
